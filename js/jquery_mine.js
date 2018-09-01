c883867ef08c045acf264c4475cab5c10ac97354
waelhamada <mr.waelhamada@gmail.com>
2018-09-01 19:18:34 +0200

    initial commit

---
+++ js/jquery_mine.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+	
+	
+$('.clicktosee').eq(0).click(function(){
+	$('.clickhide').eq(0).slideToggle(1000)	
+	
+})
+	$('.clicktosee').eq(1).click(function(){
+	$('.clickhide').eq(1).slideToggle(1000)	
+	
+})
+	$('.clicktosee').eq(2).click(function(){
+	$('.clickhide').eq(2).slideToggle(1000)	
+	
+})
+	$('.clicktosee').eq(3).click(function(){
+	$('.clickhide').eq(3).slideToggle(1000)	
+	
+})
+	$('.clicktosee').eq(4).click(function(){
+	$('.clickhide').eq(4).slideToggle(1000)	
+	
+})
+
+});
